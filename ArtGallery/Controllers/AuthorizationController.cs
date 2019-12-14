4e20bec1b7905617c06d2e9e282c790f8f324008
MuhammedAlaa <soupermado@gmail.com>
2019-12-14 19:48:01 +0200

    some cheack to sign in and sign up

--- ArtGallery/Controllers/AuthorizationController.cs
+++ ArtGallery/Controllers/AuthorizationController.cs
@@ -1,12 +1,10 @@
 ﻿using ArtGallery.Models;
 using ArtGallery.ViewModels;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using ArtGallery.DBaccess;
+using System.Data;
 
 namespace ArtGallery.Controllers
 {
@@ -16,41 +14,96 @@ namespace ArtGallery.Controllers
         // GET: Authorization
         public ActionResult Index()
         {
+            User inputUser = (User)TempData["User"];
             return View();
         }
         [HttpGet]
+        [AllowAnonymous]
         public ActionResult SignUp() {
             return View();
         }
 
         [HttpPost]
-        public ActionResult SignUp(User newuser) {
-            newuser.PROFILE_PIC = "~/Image/" + newuser.PROFILE_PIC;
-            
-            if (newuser.imagefile != null)
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(RegisterViewModel newuser) {
+            if (ModelState.IsValid)
             {
-                string imagefilename = Path.GetFileNameWithoutExtension(newuser.imagefile.FileName);
-                string extension = Path.GetExtension(newuser.imagefile.FileName);
-                imagefilename = imagefilename + DateTime.Now.ToString("yymmssfff") + extension;
-                newuser.PROFILE_PIC = "~/Image/" + imagefilename;
-                imagefilename = Path.Combine(Server.MapPath("~/Image/"), imagefilename);
-                newuser.imagefile.SaveAs(imagefilename);
-            }
-            db.SignUp(newuser);
-            ModelState.Clear();
 
-            return View();
+                if (newuser.imagefile != null)
+                {
+                    string imagefilename = Path.GetFileNameWithoutExtension(newuser.imagefile.FileName);
+                    string extension = Path.GetExtension(newuser.imagefile.FileName);
+                    imagefilename = imagefilename + DateTime.Now.ToString("yymmssfff") + extension;
+                    newuser.PROFILE_PIC = "~/Image/" + imagefilename;
+                    imagefilename = Path.Combine(Server.MapPath("~/Image/"), imagefilename);
+                    string hash = (newuser.PASSWORD);
+                    if (db.SignUp(newuser) == 0)
+                    {
+                        return RedirectToAction("SignUp", "Authorization");
+                    }
+                    else
+                        newuser.imagefile.SaveAs(imagefilename);
+                }
+
+                ModelState.Clear();
+                TempData["User"] = newuser;
+                return RedirectToAction("Index", "Authorization");
+            }
+            else
+            {
+                return View(newuser);
+            }
+
+            
         }
         
         [HttpGet]
+        [AllowAnonymous]
         public ActionResult  SignIn() {
             return View();
         }
-
         [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
         public ActionResult SignIn(LoginViewModel logeduser) {
+            if (ModelState.IsValid)
+            {
+          
+                DataTable t = db.SignIn(logeduser);
+                if (t == null)
+                    return RedirectToAction("SignIn","Authorization");
+                else
+                {
+                    string Email = Convert.ToString(t.Rows[0]["EMAIL"]);
+                    string Fname = Convert.ToString(t.Rows[0]["FNAME"]);
+                    string password = Convert.ToString(t.Rows[0]["PASSWORD"]);
+                    string Minit = Convert.ToString(t.Rows[0]["MINIT"]);
+                    string Lname = Convert.ToString(t.Rows[0]["LNAME"]);
+                    string Phone = Convert.ToString(t.Rows[0]["PHONE"]);
+                    string imagepath = Convert.ToString(t.Rows[0]["PROFILE_PIC"]);
+                    string username = Convert.ToString(t.Rows[0]["USER_NAME"]);
+                    User inputUser = new User
+                    {
+                        EMAIL = Email,
+                        USER_NAME = username,
+                        FNAME = Fname,
+                        MINIT = Minit,
+                        PASSWORD = password,
+                        LNAME = Lname,
+                        PHONE = Phone,
+                        PROFILE_PIC = imagepath
+                    };
+                    TempData["User"] = inputUser;
+                    return RedirectToAction("Index", "Authorization");
+                }
+            }
+            else
+            {
+                return View(logeduser);
+            }
 
-            return View();
+        
         }
 
 
--- ArtGallery/Models/User.cs
+++ ArtGallery/Models/User.cs
@@ -3,49 +3,30 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
-using System.ComponentModel;
 
 namespace ArtGallery.Models
 {
     public class User
     {
-        [Required(ErrorMessage = "Please enter your user name")]
-        [RegularExpression("^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$", ErrorMessage = "Please enter a valid user name")]
+
         public string USER_NAME { get; set; }
 
 
-        [Required( ErrorMessage ="Please enter your email")]
-        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail address")]
         public string EMAIL { get; set; }
 
 
-        [Required(ErrorMessage = "Please enter your password")]
-        [DataType(DataType.Password)]
-        [RegularExpression("^(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).*$", ErrorMessage = "Invalid password format")]
         public string PASSWORD { get; set; }
 
-        [Required(ErrorMessage = "Please enter your first name")]
-        [StringLength(10, ErrorMessage = "Max length = 10 characters")]
-        [RegularExpression("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$", ErrorMessage = "Only letters are allowed")]
         public string FNAME { get; set; }
 
-        [Required(ErrorMessage = "Please enter your middle name")]
-        [StringLength(1, ErrorMessage = "Max length = 1 characters")]
-        [RegularExpression("^[a-zA-Z]", ErrorMessage = "Please enter a valid minit it is one char and only letters are allowed")]
         public string MINIT { get; set; }
 
-        
 
-        [Required(ErrorMessage = "Please enter your last name")]
-        [StringLength(10, ErrorMessage = "Max length = 10 characters")]
-        [RegularExpression("^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$", ErrorMessage = "Only letters are allowed")]
+
         public string LNAME { get; set; }
 
-        [Required(ErrorMessage = "Please enter your phone number")]
-        [RegularExpression("01[0-9]{9}", ErrorMessage = "The phone number must be in format 01020809076")]
         public string PHONE { get; set; }
         public string PROFILE_PIC { get; set; }
         public HttpPostedFileBase imagefile { get; set; }
-
     }
 }
