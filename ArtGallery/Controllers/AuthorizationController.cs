c998080d639932d279b12830d4caec3ef7aea3b5
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2019-12-15 14:07:05 +0200

    sessions added

--- ArtGallery/Controllers/AuthorizationController.cs
+++ ArtGallery/Controllers/AuthorizationController.cs
@@ -132,6 +132,7 @@ namespace ArtGallery.Controllers
                 };
 
                 TempData["User"] = inputUser;
+                System.Web.Security.FormsAuthentication.SetAuthCookie(logeduser.email, false);
                 return RedirectToAction("Index", "Home", true);
             }
         }
