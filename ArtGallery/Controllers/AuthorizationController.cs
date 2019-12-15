c27c53f1fb5dd58697ee3135965c040e6e681190
MuhammedAlaa <soupermado@gmail.com>
2019-12-15 05:40:32 +0200

    merge conflict

--- ArtGallery/Controllers/AuthorizationController.cs
+++ ArtGallery/Controllers/AuthorizationController.cs
@@ -19,21 +19,24 @@ namespace ArtGallery.Controllers
         }
         [HttpGet]
         [AllowAnonymous]
-        public ActionResult SignUp() {
+        public ActionResult SignUp()
+        {
             return View();
         }
 
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public ActionResult SignUp(RegisterViewModel newuser) {
-            string imagefilename ="";
 
-            
+        public ActionResult SignUp(RegisterViewModel newuser)
+        {
+            string imagefilename = "";
+
+
             if (ModelState.IsValid)
             {
 
-                
+
                 if (newuser.imagefile != null)
                 {
                     imagefilename = Path.GetFileNameWithoutExtension(newuser.imagefile.FileName);
@@ -41,15 +44,15 @@ namespace ArtGallery.Controllers
                     imagefilename = imagefilename + DateTime.Now.ToString("yymmssfff") + extension;
                     newuser.PROFILE_PIC = "~/Image/" + imagefilename;
                     imagefilename = Path.Combine(Server.MapPath("~/Image/"), imagefilename);
-                   
-                   
+
+
                 }
                 if (db.SignUp(newuser) == 0)
                 {
                     ModelState.AddModelError("", "Invalid signup attempt.");
                     return View();
                 }
-                else if(newuser.imagefile != null)
+                else if (newuser.imagefile != null)
                     newuser.imagefile.SaveAs(imagefilename);
 
                 ModelState.Clear();
@@ -72,9 +75,8 @@ namespace ArtGallery.Controllers
                 return View(newuser);
             }
 
-            
-        }
 
+        }
 
         //this is used to check whether the name is already taken when signing up with ajax reuquest
         public JsonResult IsUserNameAvailable(string USER_NAME)
@@ -84,55 +86,63 @@ namespace ArtGallery.Controllers
 
         [HttpGet]
         [AllowAnonymous]
-        public ActionResult  SignIn() {
+        public ActionResult SignIn()
+        {
             return View();
         }
+
+
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public ActionResult SignIn(LoginViewModel logeduser) {
-            if (ModelState.IsValid)
-            {
-          
-                DataTable t = db.SignIn(logeduser);
-                if (t == null)
-                {
-                    ModelState.AddModelError("", "Invalid login attempt.");
-                    return View();
+        public ActionResult SignIn(LoginViewModel logeduser)
+        {
 
-                }
-                else
-                {
-                    string Email = Convert.ToString(t.Rows[0]["EMAIL"]);
-                    string Fname = Convert.ToString(t.Rows[0]["FNAME"]);
-                    string password = Convert.ToString(t.Rows[0]["PASSWORD"]);
-                    string Minit = Convert.ToString(t.Rows[0]["MINIT"]);
-                    string Lname = Convert.ToString(t.Rows[0]["LNAME"]);
-                    string Phone = Convert.ToString(t.Rows[0]["PHONE"]);
-                    string imagepath = Convert.ToString(t.Rows[0]["PROFILE_PIC"]);
-                    string username = Convert.ToString(t.Rows[0]["USER_NAME"]);
-                    User inputUser = new User
-                    {
-                        EMAIL = Email,
-                        USER_NAME = username,
-                        FNAME = Fname,
-                        MINIT = Minit,
-                        PASSWORD = password,
-                        LNAME = Lname,
-                        PHONE = Phone,
-                        PROFILE_PIC = imagepath
-                    };
-                    TempData["User"] = inputUser;
-                    return RedirectToAction("Index", "Authorization");
-                }
-            }
-            else
+            if (!ModelState.IsValid)
             {
                 return View(logeduser);
             }
 
-        
+            DataTable t = db.SignIn(logeduser);
+            if (t == null)
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return View(logeduser);
+            }
+            else
+            {
+                string Email = Convert.ToString(t.Rows[0]["EMAIL"]);
+                string Fname = Convert.ToString(t.Rows[0]["FNAME"]);
+                string password = Convert.ToString(t.Rows[0]["PASSWORD"]);
+                string Minit = Convert.ToString(t.Rows[0]["MINIT"]);
+                string Lname = Convert.ToString(t.Rows[0]["LNAME"]);
+                string Phone = Convert.ToString(t.Rows[0]["PHONE"]);
+                string imagepath = Convert.ToString(t.Rows[0]["PROFILE_PIC"]);
+                string username = Convert.ToString(t.Rows[0]["USER_NAME"]);
+                User inputUser = new User
+                {
+                    EMAIL = Email,
+                    USER_NAME = username,
+                    FNAME = Fname,
+                    MINIT = Minit,
+                    PASSWORD = password,
+                    LNAME = Lname,
+                    PHONE = Phone,
+                    PROFILE_PIC = imagepath
+                };
+
+                TempData["User"] = inputUser;
+                return RedirectToAction("Index", "Home", true);
+            }
         }
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
 
 
 
