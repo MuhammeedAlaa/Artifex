59d795b0e7b62b118997ea1aa3588f0a0e3aa00c
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2019-12-15 16:00:06 +0200

    edited the admin layout

--- ArtGallery/Controllers/AdminController.cs
+++ ArtGallery/Controllers/AdminController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ArtGallery.ViewModels;
 
 namespace ArtGallery.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+
         public ActionResult Index()
         {
-            return View();
+            var admin = new AdminViewModel();
+            admin.Admin.Name = "nice man";
+            return View(admin);
         }
     }
 }
---
+++ ArtGallery/ViewModels/AdminViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ArtGallery.Models;
+
+namespace ArtGallery.ViewModels
+{
+    public class AdminViewModel : ViewModelBase
+    {
+        public Admin Admin = new Admin();
+    }
+
+    public abstract class ViewModelBase
+    {
+    }
+}
